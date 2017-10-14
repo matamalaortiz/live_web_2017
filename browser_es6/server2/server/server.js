ba8f7bdb95fd6ee54a8d3e63be8432282156ccb4
Alejandro <matamala@material.cl>
2017-10-14 18:24:00 -0400

    ser

--- browser_es6/server2/server/server.js
+++ browser_es6/server2/server/server.js
@@ -38,13 +38,38 @@ app.get('/', function(req, res) {
   res.render("index.mustache")
 });
 
-io.on('connection', socket => {
-  handleSocket(socket);
-});
 
+console.log('server running in port 7000' );
 
-// SERVER OPEN IN PORT 3000
-server.listen(PORT, () => {
-  var port = 7000;
-  console.log('running at ' + port);
-});
+// WebSocket Portion
+// WebSockets work with the HTTP server
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+	// We are given a websocket object in our function
+	function (socket) {
+
+		console.log("We have a new client: " + socket.id);
+
+		// When this user emits, client side: socket.emit('otherevent',some data);
+		socket.on('chatmessage', function(data) {
+			console.log("Received: 'chatmessage' " + data);
+			io.sockets.emit('chatmessage', data);
+		});
+
+		socket.on('location', function(data) {
+			console.log("Received: location " + data);
+			io.sockets.emit('location', data);
+		});
+
+		socket.on('newlocation', function(data) {
+			console.log("Received: new location " + data);
+			io.sockets.emit('newlocation', data);
+		});
+
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected " + socket.id);
+		});
+	}
+);
