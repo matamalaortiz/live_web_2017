066aeb73f18c0647892482228e023bf151a5b66d
Alejandro <matamala@material.cl>
2017-12-02 23:24:26 -0500

    deeplearn

--- Deeplearn-Socket/server/server.js
+++ Deeplearn-Socket/server/server.js
@@ -21,7 +21,7 @@ const  mustacheExpress = require('mustache-express');
 
 
 const  bodyParser = require('body-parser');
-const  PORT = process.env.PORT || 3000;
+const  PORT = process.env.PORT || 443;
 
 // MIDDLEWARE
 app.use(bodyParser.json());
